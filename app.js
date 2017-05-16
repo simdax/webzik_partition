5532956a1159e8c2c2add050be67737019024517
simon <simoncornaz@gmail.com>
2017-05-16 10:23:30 +0200

    fix server ... 2

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-var server = http.Server(app);
 
+
+// server
+var server = http.Server(app);
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+server.listen(port);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 app.get('/', function (req, res) {
   res.sendFile( __dirname + '/index.html')
