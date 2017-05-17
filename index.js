1cecd1715fe9bc9ee7fbbf64beb8c022e14dde9c
simon <simoncornaz@gmail.com>
2017-05-17 17:37:16 +0200

    changed node start

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-//express
 var express = require('express');
 var app = express();
+var http = require('http');
+
+
+
+
 // server
-var server = app.listen(3000, function(){
-  console.log("starting on :3000");
-});
-//io
-var io = require('socket.io').listen(server);
-        // io.set('transports', ['websocket']);
+var server = http.Server(app);
+var port = normalizePort(process.env.PORT || '3050');
+app.set('port', port);
+server.listen(port);
+// server.on('listening', onListening);
 
+
+//SOCKET IO
+var io = require('socket.io')(server);
 io.on('connection', function(socket){
-  console.log("coucou");
-
-  socket.on('pingServer',(arf)=>console.log(arf))
-  socket.on('disconnect',()=>{
-    console.log('bye !');
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+});
 
-})
+function normalizePort(val) {
 
+  var port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 app.get('/', function (req, res) {
-  // res.send("ta mere");
   res.sendFile( __dirname + '/index.html')
 });
 app.use('/dist', express.static(__dirname +'/dist'));
+
+app.listen(port, function () {
+  console.log('Example app listening on port'+port) 
+})
